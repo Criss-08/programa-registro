d21d7a8c8a02c2c954934bd378d8c501199f9b10
Escalante Cristian <criss.escalante08@gmail.com>
2026-06-15 23:17:25 -0300

    Unitial Spring Boot project setup

---
+++ src/main/java/com/cristian/programaregistro/ProgramaregistroApplication.java
@@ -0,0 +1,13 @@
+package com.cristian.programaregistro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramaregistroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramaregistroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cristian/programaregistro/ProgramaregistroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristian.programaregistro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramaregistroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
